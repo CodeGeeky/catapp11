133eccd98d767cee5b35e04dfb76890ca16468b5
BlackRabbitt <sshakya.mail@gmail.com>
2013-11-29 21:49:51 +0545

    Login/Signup improved_1.0.0

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
     user = User.authenticate(params[:phone], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in"
+      redirect_to root_url #, :notice => "Logged in"
     else
       
       flash.now.alert = "Invalid phone or password"
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged Out"
+    redirect_to root_url #, :notice => "Logged Out"
   end
   
 end
