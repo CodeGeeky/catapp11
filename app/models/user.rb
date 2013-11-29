a1f1aa60a9d7e7a5a3019ba4d1d2342052675a99
BlackRabbitt <sshakya.mail@gmail.com>
2013-11-30 02:15:25 +0545

    validation_1.0.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,27 @@ class User < ActiveRecord::Base
   attr_accessor :password
   before_save :encrypt_password
 
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  validates_uniqueness_of :phone
-  validates_presence_of :phone
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40},
+                       :on => :create
+  validates :email, :presence => true, 
+                    :uniqueness => true          
+  validate :email_regex
   
+  validates :phone, :presence => true,
+                    :uniqueness => true,
+                    :length => { is: 10 },
+                    numericality: true
+                    
+  validates :name, :presence =>true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  
+  def email_regex
+    if email.present? and not email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+      errors.add :email, "This is not a valid email format"
+    end
+  end
+
 
   def self.authenticate(phone, password)
     user = find_by_phone(phone)
