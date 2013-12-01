e130bd26ec4c98b9094d1748b3cf2a54043a4ca5
BlackRabbitt <sshakya.mail@gmail.com>
2013-12-01 12:11:31 +0545

    update109

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,8 +21,8 @@ class PagesController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    #redirect_to root_url #, :notice => "Logged Out"
-    render 'welcome/index'
+    redirect_to root_url #, :notice => "Logged Out"
+    #render 'welcome/index'
   end
   
 end
