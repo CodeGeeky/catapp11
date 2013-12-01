5ee08763d95a48e56e2a545aac9925edf74838be
BlackRabbitt <sshakya.mail@gmail.com>
2013-12-01 19:23:44 +0545

    update_1.1.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
                        :length => {:within => 6..40},
                        :on => :create
   validates :email, :presence => true, 
-                    :uniqueness => true          
-  validate :email_regex
+                    :uniqueness => true,
+                    :format => { :with => %r{.+@.+\..+} }        
   
   validates :phone, :presence => true,
                     :uniqueness => true,
@@ -17,12 +17,6 @@ class User < ActiveRecord::Base
                     
   validates :name, :presence =>true, format: { without: /\d/} 
     #with: /^[0-9`!@#\$%\^&*+_=]+$/, message: "only allows letters" }
-  
-  def email_regex
-    if email.present? and not email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
-      errors.add :email, "This is not a valid email format"
-    end
-  end
 
 
   def self.authenticate(phone, password)
