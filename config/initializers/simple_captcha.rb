8e6f1e6c596a2543511a515dea82bb5f36b87e05
BlackRabbitt <sshakya.mail@gmail.com>
2013-12-12 22:43:54 +0545

    nothing

--- config/initializers/simple_captcha.rb
+++ config/initializers/simple_captcha.rb
@@ -1,8 +1,11 @@
 SimpleCaptcha.setup do |sc|
-  sc.image_style = 'almost_invisible'
-  sc.add_image_style('mycaptha', [
-      "-background '#F4F7F8'",
-      "-fill '#86818B'",
-      "-border 1",
-      "-bordercolor '#E0E2E3'"])
+  sc.image_size = '150x30'
+  sc.length = 6
+  sc.image_style = 'random'
+  
+  #sc.add_image_style('mycaptha', [
+   #   "-background '#F4F7F8'",
+    #  "-fill '#86818B'",
+     # "-border 1",
+      #"-bordercolor '#E0E2E3'"])
 end
