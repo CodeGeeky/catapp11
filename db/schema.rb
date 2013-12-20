0a112858302071c7b84f698cb76eb0f58445abf2
BlackRabbitt <sshakya.mail@gmail.com>
2013-12-21 02:12:34 +0545

    menu model defined

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213141529) do
+ActiveRecord::Schema.define(version: 20131220202517) do
+
+  create_table "menus", force: true do |t|
+    t.string   "item_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "quantity"
+  end
 
   create_table "simple_captcha_data", force: true do |t|
     t.string   "key",        limit: 40
