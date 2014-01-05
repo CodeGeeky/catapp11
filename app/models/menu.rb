b4525f567ac32372c9bb9de5e80c580e38e016eb
BlackRabbitt <sshakya.mail@gmail.com>
2014-01-06 01:15:54 +0545

    menu show modified

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,8 +1,8 @@
 class Menu < ActiveRecord::Base
   has_attached_file :photo, :styles => {:small => "250x150"},
                     :url => "/assets/slide/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/slide/:id/:style/:basename.:extension"
-  
+                    :path => ":rails_root/public/assets/slide/:id/:style/:basename.:extension",
+                    :default_url => "/images/no-picture.png"
   
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']   
