77236425975c179a3512a17870f81501ab92d90a
BlackRabbitt <sshakya.mail@gmail.com>
2014-02-20 10:21:05 +0545

    updateV1.0.2

--- app/models/hcontent.rb
+++ app/models/hcontent.rb
@@ -1,5 +1,5 @@
 class Hcontent < ActiveRecord::Base
-  attr_accessor :title, :subtitle, :content
+  # attr_accessor :title, :subtitle, :content
   attr_accessible :title, :subtitle, :content
   validates :title, :presence => true
   validates :subtitle, :presence => true
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,13 +1,15 @@
 class Slide < ActiveRecord::Base
-   has_attached_file :photo, :styles => {:medium => "960x655>"},
-                    :url => "/assets/slide/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/slide/:id/:style/:basename.:extension"
-  
-  
-  validates_attachment_size :photo, :less_than => 5.megabytes
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
-  
-  validates :title, :presence => true
-  validates :description, :presence => true
-  validates :photo, :presence => true
+	attr_accessible :title, :description, :photo
+
+	has_attached_file :photo, :styles => {:medium => "960x655>"},
+	            :url => "/assets/slide/:id/:style/:basename.:extension",
+	            :path => ":rails_root/public/assets/slide/:id/:style/:basename.:extension"
+
+
+	validates_attachment_size :photo, :less_than => 5.megabytes
+	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+
+	validates :title, :presence => true
+	validates :description, :presence => true
+	validates :photo, :presence => true
 end
